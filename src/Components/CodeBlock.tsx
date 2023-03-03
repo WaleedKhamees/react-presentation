750a2883dc6f29640f0e513a73e0d90b4de32ecd
Walid-kh <walid.osama.khamees@gmail.com>
2023-03-03 15:26:49 +0200

    Feat: Code highlighting

--- src/Components/CodeBlock.tsx
+++ src/Components/CodeBlock.tsx
@@ -1,17 +1,24 @@
-import { ReactNode } from "react";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import dark from "react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark";
 
 export const CodeBlock = ({
   children,
   fileName,
 }: {
-  children: ReactNode;
+  children: string;
   fileName?: string;
 }) => {
   return (
-    <div className="flex flex-col items-center border border-white rounded-lg max-w-full max-h-[50%]">
-      <pre className="text-white border-b w-full overflow-scroll">
-        <code className="m-4 inline-block">{children}</code>
-      </pre>
+    <div className="flex flex-col items-center border border-white rounded-lg md:max-w-[50%] w-full max-h-[50%] overflow-clip">
+      <div className="border-b w-full overflow-scroll">
+        <SyntaxHighlighter
+          language="typescript"
+          style={dark}
+          customStyle={{ background: "transparent" }}
+        >
+          {children}
+        </SyntaxHighlighter>
+      </div>
       <p className="font-mono w-full text-center py-2 text-body">
         {fileName ?? "unnamed"}
       </p>
